df33b09c82b2a6e2aac9879d705c0943bb06779e
SayQuit <501241011@qq.com>
2022-08-14 20:50:42 +0800

    20220814 2

--- pages/category/index.js
+++ pages/category/index.js
@@ -12,7 +12,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // wx.request({
+    //   url: 'http://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success:(result)=>{
+    //     console.log(result);
+    //     // 
+    //   }
+    // })
   },
 
   /**
