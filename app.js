ea1f1cb7e0e145d8a83de433fb2a0c173437db9c
SayQuit <501241011@qq.com>
2022-08-15 12:59:22 +0800

    纠正

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 // app.js
 App({
-  
+  // onLaunch() {
+  //   // 展示本地存储能力
+  //   const logs = wx.getStorageSync('logs') || []
+  //   logs.unshift(Date.now())
+  //   wx.setStorageSync('logs', logs)
+
+  //   // 登录
+  //   wx.login({
+  //     success: res => {
+  //       // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //     }
+  //   })
+  // },
+  // globalData: {
+  //   userInfo: null
+  // }
+
+  onLaunch(){
+    // 小程序启动时获取用户信息
+    console.log('onLaunch');
+  },
+  onShow(){
+    // 切页面回到小程序时触发
+    console.log('onShow');
+  },
+  onHide(){
+    // 切页面出去时触发
+    console.log('onHide');
+  },
+  onError(err){
+    // 
+    console.log('onError');
+    console.log(err);
+  },
+  onPageNotFound(err){
+    // 找不到第一个入口页面触发
+    console.log('onPageNotFound');
+    console.log(err);
+  }
 })
---
+++ pages/demo01/demo01.js
@@ -0,0 +1,38 @@
+// pages/demo01/demo01.js
+Page({
+  data: {
+    msg:'你好',
+    isChecked:true,
+
+    person:[
+      {
+        id:1,
+        name:'小明',
+        sex:'男',
+        hobby:'足球',
+        age:19
+      },
+      {
+        id:2,
+        name:'小光',
+        sex:'男',
+        hobby:'篮球',
+        age:18
+      },
+      {
+        id:3,
+        name:'小红',
+        sex:'女',
+        hobby:'排球',
+        age:20
+      }
+    ],
+
+    jack:{
+      age:18,
+      name:'jack',
+      sex:'男'
+    }
+  }
+})
+
